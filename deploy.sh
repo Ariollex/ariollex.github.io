c19140d15f485737d10c94322c6fb85331cfe82a
Ariollex <ariollex@gmail.com>
2022-07-02 22:30:47 +0300

    Add script for deploy

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+export source=$(pwd)
+
+# Clone website
+git clone https://github.com/ariollex/ariollex.github.io -b main ~/public 2>/dev/null
+
+# Build
+hugo -d ../public
+
+# Deploy changes
+echo -e "\033[0;32mDeploying changes to GitHub...\033[0m"
+cd ~/public
+
+# Commit changes.
+git commit -am "[ArCI]: Push Built Site"
+
+# Push source and build repos.
+git push https://"$GH_PERSONAL_TOKEN"@github.com/Ariollex/ariollex.github.io.git main
+
+if [[ $1 != "-k" ]]; then
+  rm -rf ~/public
+fi
+
+echo -e "\033[0;32mSuccess!\033[0m"
+# Back to source directory
+cd $source
